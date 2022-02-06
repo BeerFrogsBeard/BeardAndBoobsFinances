eeef4258c498051d2c92830447543a87dac341c2
beerfrogsbeard <jeremiah.larson@gmail.com>
2022-02-06 09:53:07 -0500

    Updates

--- BeardAndBoobsFinances/Program.cs
+++ BeardAndBoobsFinances/Program.cs
@@ -7,31 +7,50 @@ var builder = WebApplication.CreateBuilder(args);
 builder.ConfigureServices();
 
 //code for serilog
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
 Log.Logger = new LoggerConfiguration()
-.Enrich.FromLogContext()
-.WriteTo.File(@"C:\logs\log.txt")
+.ReadFrom.Configuration(configuration)
 .CreateLogger();
 
 builder.Host.UseSerilog();
 //end code for serilog
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Information("Application Starting Up");
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseSerilogRequestLogging();
+
+    app.UseRouting();
+
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "The application failed to start correctly... Fucking A-a-ron or Phil!");
+}
+finally 
+{ 
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
 
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
